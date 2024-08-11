27d4eb8e33bb09a091432324842609aa1c301e82
abstruse-scientia <priyanshu0496@gmail.com>
2024-08-11 20:29:45 +0530

    refactor: Refactor code

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,5 +1,8 @@
 let humanScore = 0;
-let computeScore = 0;
+let computerScore = 0;
+
+
+
 
 
 function getComputerChoice(){
@@ -15,41 +18,70 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let user_choice = prompt("Enter your choice: ")
-    return user_choice
+
+function humanChoice(event){
+    let target = event.target;
+    let computer = getComputerChoice();
+    console.log(target.id);
+    switch(target.id){
+        case "rock":
+            playRound("rock", computer);
+            break;
+        case "paper":
+            playRound("rock", computer);
+            break;
+        case "scissors":
+            playRound("scissors", computer);
+            break;
+    }
 }
+
+function checkScore(player, computer){
+    let text = document.querySelector("#result");
+    if (player == 5){
+        text.textContent = 'You win . Click to play again.';
+        humanScore = 0;
+        computerScore = 0;
+    }
+    if(computer == 5){
+        text.textContent = 'You lose. Click to play again.';
+        humanScore = 0;
+        computerScore = 0;
+    }
+}
+
+
+let choice = document.querySelector('.bottom');
+choice.addEventListener('click', humanChoice);
+
 
 
 function playRound(humanChoice, computerChoice){
-    humanChoice.toLowerCase()
+    humanChoice.toLowerCase();
+    let result = document.querySelector("#result");
+    let player_score = document.querySelector("#human_score")
+    let computer_score = document.querySelector("#computer_score")
     if (humanChoice == "rock" && computerChoice == "scissors"){
         humanScore += 1;
-        console.log(`You win. ${humanChoice} beats ${computerChoice}`)
+        result.textContent = `You win. ${humanChoice} beats ${computerChoice}`
     }
     else if (humanChoice == "paper" && computerChoice == "rock"){
         humanScore += 1;
-        console.log(`You win. ${humanChoice} beats ${computerChoice}`)
+        result.textContent = `You win. ${humanChoice} beats ${computerChoice}`
     }
     else if(humanChoice == "scissors" && computerChoice == "paper"){
         humanScore += 1;
-        console.log(`You win. ${humanChoice} beats ${computerChoice}`)
+        result.textContent = `You win. ${humanChoice} beats ${computerChoice}`
     }
     else if(humanChoice == computerChoice){
-        console.log(`Game draw. ${humanChoice} draws ${computerChoice}`)
+        result.textContent = `Game draw. ${humanChoice} draws ${computerChoice}`
     }
     else {
-        computeScore += 1;
-        console.log(`You lose. ${computerChoice} beats ${humanChoice}`)
+        computerScore += 1;
+        result.textContent = `You lose. ${computerChoice} beats ${humanChoice}`
     }
-    console.log(`Human score: ${humanScore} \t Computer score: ${computeScore}`)
+    player_score.textContent = (`Your score: ${humanScore}`);
+    computer_score.textContent = (`Computer score: ${computerScore}`);
+    checkScore(humanScore, computerScore);
 }
 
-
-for(let i=0;i<5;i++){
-    const human = getHumanChoice();
-    const computer = getComputerChoice();
-
-    playRound(human, computer);
-
-}
